843d08522336b6d5cf7b1f96f7b20d6e81d67f9c
Manvi Kapoor <kapoor.manvi11@gmail.com>
2020-08-23 05:47:18 +0000

    Validates both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Enter store name"
+
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+
+new_store.save!
