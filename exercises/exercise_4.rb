678e5e69e3a67858d6fdc4f565cb7fc93a80daae
Manvi Kapoor <kapoor.manvi11@gmail.com>
2020-08-23 00:40:56 +0000

    Loads stores and differentiates by type of apparel carried whil displaying their annual revenue.

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true, mens_apparel: false)
+
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, womens_apparel: false, mens_apparel: true)
+
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, womens_apparel: true, mens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |mens_store|
+  puts "#{mens_store.name} is a mens apparel store that generates $#{mens_store.annual_revenue} in revenue each year."
+end
+
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+
+@womens_stores.each do |womens_store|
+  puts "#{womens_store.name} is a womens apparel store that generates $#{womens_store.annual_revenue} in revenue each year."
+end
+
